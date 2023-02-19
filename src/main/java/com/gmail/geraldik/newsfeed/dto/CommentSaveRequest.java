cbd396291746f5bac87ce5d49881e333ee7e2402
vlad <geraldik1986@gmail.com>
2023-02-19 12:56:47 +0300

    Add @NotNull validation

--- src/main/java/com/gmail/geraldik/newsfeed/dto/CommentSaveRequest.java
+++ src/main/java/com/gmail/geraldik/newsfeed/dto/CommentSaveRequest.java
@@ -1,15 +1,18 @@
 package com.gmail.geraldik.newsfeed.dto;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentSaveRequest {
 
+    @NotNull(message = "The name of commentator must be not null")
     @Size(min = 3, max = 30, message = "The name of commentator must be in range of 3-300 characters")
-    @Size()
-    private String  commentator;
+    private String commentator;
+    @NotNull(message = "The comment must be not null")
     @Size(min = 1, max = 500, message = "Comment length must be in the range of 1-500 characters")
-    private String  body;
+    private String body;
+    @NotNull(message = "The id of item must be not null")
     private Integer itemId;
 }
